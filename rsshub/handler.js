9b249f2ff671e26e79b02838f7016380ff95e9d0
rksvc <rksvc@qq.com>
2024-09-30 12:39:36 +0800

    validate rss item date

--- rsshub/handler.js
+++ rsshub/handler.js
@@ -1,7 +1,16 @@
 ;async (sourcePath, ctx) => {
   const data = await require(`./${sourcePath}`).route.handler(ctx)
-  for (const item of data.item ?? [])
-    for (const key of ['pubDate', 'updated'])
-      if (item[key]) item[key] = new Date(item[key]).toISOString()
+  for (const item of data.item ?? []) {
+    for (const key of ['pubDate', 'updated']) {
+      if (item[key]) {
+        const date = new Date(item[key])
+        if (Number.isNaN(date.valueOf())) {
+          delete item[key]
+        } else {
+          item[key] = date.toISOString()
+        }
+      }
+    }
+  }
   return data
 }
