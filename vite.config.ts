757bda763f238ca743b587e7b900e5727c7a56a0
rksvc <rksvc@qq.com>
2024-11-01 18:28:47 +0800

    exclude blueprintjs icon loaders

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    reportCompressedSize: false,
+    rollupOptions: {
+      external: [
+        './paths-loaders/allPathsLoader',
+        './paths-loaders/splitPathsBySizeLoader',
+      ],
+    },
+  },
   server: {
     proxy: {
       '^/(?!(@|node_modules|web)).+': 'http://localhost:1234',
