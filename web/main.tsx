ba52c54ce09f029a01bbc8066187ea54041459db
rksvc <rksvc@qq.com>
2024-11-23 22:31:20 +0800

    polish dialog style

--- web/main.tsx
+++ web/main.tsx
@@ -8,6 +8,7 @@ const style = document.querySelector<HTMLHtmlElement>(':root')!.style
 style.setProperty('--black', Colors.BLACK)
 style.setProperty('--gray1', Colors.GRAY1)
 style.setProperty('--dark-gray3', Colors.DARK_GRAY3)
+style.setProperty('--dark-gray4', Colors.DARK_GRAY4)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <OverlaysProvider>
