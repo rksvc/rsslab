68e35efa334f631074bae6d01b28e61a874c2df0
rksvc <rksvc@qq.com>
2024-11-24 01:55:14 +0800

    fix dialog style

--- web/main.tsx
+++ web/main.tsx
@@ -7,6 +7,7 @@ import './styles.css'
 const style = document.querySelector<HTMLHtmlElement>(':root')!.style
 style.setProperty('--black', Colors.BLACK)
 style.setProperty('--gray1', Colors.GRAY1)
+style.setProperty('--light-gray5', Colors.LIGHT_GRAY5)
 style.setProperty('--dark-gray3', Colors.DARK_GRAY3)
 style.setProperty('--dark-gray4', Colors.DARK_GRAY4)
 
