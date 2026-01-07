ba79c0e4ab6970f0f7efec6f0b6434897d43c2c1
rksvc <rksvc@qq.com>
2026-01-07 17:26:52 +0800

    trim spaces in rss item content

--- rss/rss.go
+++ rss/rss.go
@@ -82,7 +82,7 @@ func ParseRSS(r io.Reader) (*Feed, error) {
 			Date:     parseDate(cmp.Or(item.DublinCoreDate, item.PubDate, item.Torrent.PubDate)),
 			URL:      cmp.Or(item.OrigLink, item.Link, permalink),
 			Title:    strings.TrimSpace(item.Title),
-			Content:  cmp.Or(item.ContentEncoded, item.Description),
+			Content:  cmp.Or(strings.TrimSpace(item.ContentEncoded), strings.TrimSpace(item.Description)),
 			AudioURL: podcastURL,
 		})
 	}
