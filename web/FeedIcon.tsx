43ac5ce2d6852ddd9d86bbca89f0da3af8a75623
rksvc <rksvc@qq.com>
2026-01-12 21:32:36 +0800

    polishing

--- web/FeedIcon.tsx
+++ web/FeedIcon.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Rss } from 'react-feather'
 import { useMyContext } from './Context.tsx'
 import type { Feed } from './types.ts'
@@ -6,20 +6,18 @@ import { length, xfetch } from './utils.ts'
 
 export default function FeedIcon({ feed }: { feed: Feed }) {
   const { setFeeds } = useMyContext()
-  const [hasIcon, setHasIcon] = useState(feed.has_icon)
   const src = `api/feeds/${feed.id}/icon`
 
   useEffect(() => {
     ;(async () => {
       if (feed.has_icon == null) {
         const hasIcon = await xfetch<boolean>(`api/feeds/${feed.id}/has_icon`)
-        setHasIcon(hasIcon)
         setFeeds(feeds => feeds?.map(f => (f.id === feed.id ? { ...f, has_icon: hasIcon } : f)))
       }
     })()
   }, [feed, setFeeds])
 
-  return hasIcon ? (
+  return feed.has_icon ? (
     <img alt="feed icon" style={{ width: length(4), aspectRatio: '1/1', marginRight: '7px' }} src={src} />
   ) : (
     <span style={{ display: 'flex' }}>
