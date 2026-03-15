cf14a595bd9d9a7c681a668fd41a09072905782d
rksvc <rksvc@qq.com>
2026-03-15 10:18:16 +0800

    refactor

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"cmp"
+	"compress/gzip"
 	"errors"
 	"fmt"
 	"io"
@@ -76,6 +77,33 @@ func New(db *storage.Storage) *Server {
 	}
 
 	return s
+}
+
+func wrap(handleFunc func(context) error) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gz := &gzipResponseWriter{out: gzip.NewWriter(w), src: w}
+			defer func() {
+				if err := gz.out.Close(); err != nil {
+					log.Print(err)
+				}
+			}()
+			w = gz
+		}
+		if err := handleFunc(context{w, r}); err != nil {
+			log.Printf("%s %s: %s", r.Method, r.URL.EscapedPath(), err)
+			if _, ok := err.(*errBadRequest); ok {
+				w.WriteHeader(http.StatusBadRequest)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			_, err = w.Write(utils.StringToBytes(err.Error()))
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}
 }
 
 func (s *Server) Start(addr string) error {
